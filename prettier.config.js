6893e6736c93b20d345f31242f66d184492c0523
tianhuoDD <2434260208@qq.com>
2025-05-08 16:56:10 +0800

    chore(vscode): 更新 VSCode 设置并调整代码格式化配置
    - 在 .vscode/settings.json 中添加 njsproj 和 ntvs 到格式化排除列表
    - 更新 opus-mart 子模块版本
    - 修改 prettier.config.js 中的 useTabs 配置项注释

--- prettier.config.js
+++ prettier.config.js
@@ -4,7 +4,7 @@ export default {
 	printWidth: 120,
 	semi: true, // 在语句末尾添加分号
 	jsxSingleQuote: true, // 在JSX中使用单引号
-	useTabs: true, // 不使用tab来缩进
+	useTabs: true, // 使用tab来缩进
 	tabWidth: 2,
 	endOfLine: "auto", // 自动检测并使用正确的换行符（适应操作系统）
 };
